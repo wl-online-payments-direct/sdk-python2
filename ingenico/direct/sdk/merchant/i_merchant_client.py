1c5c08fe2281aa99bfe8e8e031071600cb3be11d
Ingencio dev team <60233882+ingenico-dev-team@users.noreply.github.com>
2021-10-19 17:30:24 +0200

    Release version 1.10.0.

--- ingenico/direct/sdk/merchant/i_merchant_client.py
+++ ingenico/direct/sdk/merchant/i_merchant_client.py
@@ -12,56 +12,11 @@ class IMerchantClient:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def products(self):
+    def hosted_checkout(self):
         """
-        Resource /v2/{merchantId}/products
+        Resource /v2/{merchantId}/hostedcheckouts
 
-        :return: :class:`ingenico.direct.sdk.merchant.products.i_products_client.IProductsClient`
-        """
-        pass
-
-    @abstractmethod
-    def sessions(self):
-        """
-        Resource /v2/{merchantId}/sessions
-
-        :return: :class:`ingenico.direct.sdk.merchant.sessions.i_sessions_client.ISessionsClient`
-        """
-        pass
-
-    @abstractmethod
-    def payouts(self):
-        """
-        Resource /v2/{merchantId}/payouts
-
-        :return: :class:`ingenico.direct.sdk.merchant.payouts.i_payouts_client.IPayoutsClient`
-        """
-        pass
-
-    @abstractmethod
-    def payments(self):
-        """
-        Resource /v2/{merchantId}/payments
-
-        :return: :class:`ingenico.direct.sdk.merchant.payments.i_payments_client.IPaymentsClient`
-        """
-        pass
-
-    @abstractmethod
-    def services(self):
-        """
-        Resource /v2/{merchantId}/services
-
-        :return: :class:`ingenico.direct.sdk.merchant.services.i_services_client.IServicesClient`
-        """
-        pass
-
-    @abstractmethod
-    def product_groups(self):
-        """
-        Resource /v2/{merchantId}/productgroups
-
-        :return: :class:`ingenico.direct.sdk.merchant.productgroups.i_product_groups_client.IProductGroupsClient`
+        :return: :class:`ingenico.direct.sdk.merchant.hostedcheckout.i_hosted_checkout_client.IHostedCheckoutClient`
         """
         pass
 
@@ -75,19 +30,64 @@ class IMerchantClient:
         pass
 
     @abstractmethod
+    def payments(self):
+        """
+        Resource /v2/{merchantId}/payments
+
+        :return: :class:`ingenico.direct.sdk.merchant.payments.i_payments_client.IPaymentsClient`
+        """
+        pass
+
+    @abstractmethod
+    def payouts(self):
+        """
+        Resource /v2/{merchantId}/payouts
+
+        :return: :class:`ingenico.direct.sdk.merchant.payouts.i_payouts_client.IPayoutsClient`
+        """
+        pass
+
+    @abstractmethod
+    def product_groups(self):
+        """
+        Resource /v2/{merchantId}/productgroups
+
+        :return: :class:`ingenico.direct.sdk.merchant.productgroups.i_product_groups_client.IProductGroupsClient`
+        """
+        pass
+
+    @abstractmethod
+    def products(self):
+        """
+        Resource /v2/{merchantId}/products
+
+        :return: :class:`ingenico.direct.sdk.merchant.products.i_products_client.IProductsClient`
+        """
+        pass
+
+    @abstractmethod
+    def services(self):
+        """
+        Resource /v2/{merchantId}/services
+
+        :return: :class:`ingenico.direct.sdk.merchant.services.i_services_client.IServicesClient`
+        """
+        pass
+
+    @abstractmethod
+    def sessions(self):
+        """
+        Resource /v2/{merchantId}/sessions
+
+        :return: :class:`ingenico.direct.sdk.merchant.sessions.i_sessions_client.ISessionsClient`
+        """
+        pass
+
+    @abstractmethod
     def tokens(self):
         """
         Resource /v2/{merchantId}/tokens
 
         :return: :class:`ingenico.direct.sdk.merchant.tokens.i_tokens_client.ITokensClient`
-        """
-        pass
-
-    @abstractmethod
-    def hosted_checkout(self):
-        """
-        Resource /v2/{merchantId}/hostedcheckouts
-
-        :return: :class:`ingenico.direct.sdk.merchant.hostedcheckout.i_hosted_checkout_client.IHostedCheckoutClient`
         """
         pass
