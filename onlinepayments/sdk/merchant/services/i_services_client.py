663b71d8cfe148d04d3265ecac4c38ddfa3cc839
Worldline Direct support team <82139942+worldline-direct-support-team@users.noreply.github.com>
2023-05-30 15:05:19 +0200

    Release version 4.10.0.

--- onlinepayments/sdk/merchant/services/i_services_client.py
+++ onlinepayments/sdk/merchant/services/i_services_client.py
@@ -31,6 +31,26 @@ class IServicesClient:
         pass
 
     @abstractmethod
+    def get_dcc_rate_inquiry(self, body, context=None):
+        """
+        Resource /v2/{merchantId}/services/dccrate - Get Dcc Rate Inquiry Api
+
+
+        :param body: :class:`onlinepayments.sdk.domain.currency_conversion_request.CurrencyConversionRequest`
+        :param context: :class:`onlinepayments.sdk.call_context.CallContext`
+        :return: :class:`onlinepayments.sdk.domain.currency_conversion_response.CurrencyConversionResponse`
+        :raise: ValidationException if the request was not correct and couldn't be processed (HTTP status code 400)
+        :raise: AuthorizationException if the request was not allowed (HTTP status code 403)
+        :raise: ReferenceException if an object was attempted to be referenced that doesn't exist or has been removed,
+                   or there was a conflict (HTTP status code 404, 409 or 410)
+        :raise: PaymentPlatformException if something went wrong at the payment platform,
+                   the payment platform was unable to process a message from a downstream partner/acquirer,
+                   or the service that you're trying to reach is temporary unavailable (HTTP status code 500, 502 or 503)
+        :raise: ApiException if the payment platform returned any other error
+        """
+        pass
+
+    @abstractmethod
     def get_privacy_policy(self, query, context=None):
         """
         Resource /v2/{merchantId}/services/privacypolicy - Get Privacy Policy
